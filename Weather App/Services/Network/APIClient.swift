4cb6729b1fc7c26e2654d17235037b55d1a96f69
Kittipod Lambangchang <tonkittipod@gmail.com>
2025-12-29 02:30:22 +0700

    feat: add api endpoint

--- Weather App/Services/Network/APIClient.swift
+++ Weather App/Services/Network/APIClient.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+    case DELETE
+    case PUT
+    case PATCH
+}
+
+struct RequestParameters {
+    var body: [String: Any]?
+    var queryItems: [URLQueryItem]?
+}
+
+protocol Endpoint {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String: String]? { get }
+    var requestParameters: RequestParameters? { get }
+}
+
+extension Endpoint {
+    var headers: [String: String]? {
+        return nil
+    }
+    
+    var requestParameters: RequestParameters? {
+        return nil
+    }
+}
--- Weather App/Services/Network/Endpoint.swift
+++ Weather App/Services/Network/Endpoint.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+enum WeatherEndpoint: Endpoint {
+    case current(city: String)
+    case forecast(city: String)
+    case airPollution(lat: Double, lon: Double)
+    
+    var baseURL: String {
+        "https://api.openweathermap.org/"
+    }
+    
+    var path: String {
+        switch self {
+        case .current:
+            return "/data/2.5/weather"
+        case .forecast:
+            return "/data/2.5/forecast"
+        case .airPollution:
+            return "/data/2.5/air_pollution"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        default:
+            return .GET
+        }
+    }
+    
+    var requestParameters: RequestParameters? {
+        switch self {
+        case .current(let city):
+            return RequestParameters(queryItems: [
+                URLQueryItem(name: "q", value: city),
+                URLQueryItem(name: "app_id", value: "API_KEY")
+                
+            ])
+        case .forecast(let city):
+            return RequestParameters(queryItems: [
+                URLQueryItem(name: "q", value: city),
+            ])
+        case .airPollution(lat: let lat, lon: let lon):
+            return RequestParameters(queryItems: [
+                URLQueryItem(name: "lat", value: "\(lat)"),
+                URLQueryItem(name: "lon", value: "\(lon)"),
+            ])
+            
+        }
+    }
+}
